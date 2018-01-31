6b594121270c835f3bf3e32290e83c764fcd637e
civic-lark <harlohasaburner@gmail.com>
2018-01-31 10:09:22 -0500

    cleanup

--- carrier_debloat.py
+++ carrier_debloat.py
@@ -1,12 +1,11 @@
 import os
 from sys import argv, exit
 
-def debloat_moto_e4(kill_file):
+def debloat_phone(kill_file):
 	try:
-		with open(kill_file, 'rb') as AL:
-			for app in AL.readlines():
-				cmd = "adb shell pm uninstall -k --user 0 %s" % app.strip()
-				os.system(cmd)
+		with open(kill_file, 'rb') as KF:
+			for app in KF.readlines():
+				os.system("adb shell pm uninstall -k --user 0 %s" % app.strip())
 
 		return True
 
@@ -16,5 +15,5 @@ def debloat_moto_e4(kill_file):
 	return False
 
 if __name__ == "__main__":
-	debloat = debloat_moto_e4(argv[1])
+	debloat = debloat_phone(argv[1])
 	exit(0 if debloat else -1)
